26ced0a86a38230a36216dcca3a4f115563c901b
deon-gracias <deongracias1@gmail.com>
2023-05-16 01:29:48 +0530

    Changed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,24 @@
+import { socials } from "../data/portfolio";
+
 export default function Footer() {
   return (
-    <footer className="p-10 footer text-base-content">
+    <footer className="p-10 footer text-base-content bg-base-200">
       <div className="flex flex-col items-center w-full">
-        <span className="mr-2">Deon Gracias</span> &copy;{" "}
-        {new Date().getFullYear()} All Rights Reserved
+        <div className="flex">
+          {socials.map((social) => (
+            <a
+              className="flex items-center gap-1 px-3"
+              href={social.href}
+              key={social.href}
+            >
+              {social.icon}
+            </a>
+          ))}
+        </div>
+
+        <div className="text-xl">Deon Gracias</div>
+
+        <div>&copy; {new Date().getFullYear()} All Rights Reserved</div>
       </div>
     </footer>
   );
