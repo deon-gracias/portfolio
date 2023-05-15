51a4a11bdc4cb566338ef898f87228b52a132eba
deon-gracias <deongracias1@gmail.com>
2023-05-15 20:53:22 +0530

    Added data

---
+++ data/portfolio.tsx
@@ -0,0 +1,34 @@
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+
+export const educations = [
+  {
+    name: "Fr. Conceicao Rodrigues College of Engineering",
+    degree: "Bachelor of Engineering",
+    field: "Computer Engineering",
+    start: "September 2021",
+    end: "May 2024",
+    pursuing: true,
+    grade: "9.2 CGPA",
+  },
+  {
+    name: "Thakur Polytechnic",
+    degree: "Diploma",
+    field: "Information Technology",
+    start: "August 2019",
+    end: "August 2021",
+    grade: "97 %",
+  },
+];
+
+export const socials = [
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/deongracias",
+    icon: <IconBrandLinkedin />,
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/deon-gracias",
+    icon: <IconBrandGithub />,
+  },
+];
---
+++ data/site.tsx
@@ -0,0 +1,6 @@
+export const nav_items = [
+  { name: "Home", href: "#" },
+  { name: "Experience", href: "#experience" },
+  { name: "Education", href: "#education" },
+  { name: "Projects", href: "#projects" },
+];
