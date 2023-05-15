eb1fdcdd56203f0747b1aaae68977f09e864674d
deon-gracias <deongracias1@gmail.com>
2023-05-15 20:54:03 +0530

    Added skill badges

---
+++ data/skill-badges.tsx
@@ -0,0 +1,37 @@
+const skill_badges = {
+  c: "https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white",
+  cplusplus:
+    "https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white",
+  java: "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white",
+  php: "https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white",
+  python:
+    "https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white",
+  html: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+  css: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
+  javascript:
+    "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black",
+  typescript:
+    "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+  nodejs:
+    "https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white",
+  go: "https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white",
+  dart: "https://img.shields.io/badge/Dart-0175C2?style=for-the-badge&logo=dart&logoColor=white",
+  reactjs:
+    "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+  nextjs:
+    "https://img.shields.io/badge/Next.js-000000.svg?style=for-the-badge&logo=Next.js&labelColor=000000",
+  mysql:
+    "https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white",
+  postgresql:
+    "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white",
+  firebase:
+    "https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase",
+  mongodb:
+    "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+  flask:
+    "https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white",
+  flutter:
+    "https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=flutter&logoColor=white",
+};
+
+export default skill_badges;
