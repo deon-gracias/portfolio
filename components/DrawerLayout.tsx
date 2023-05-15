86d2b157af881471eaa799f1f6a02a1ad1ada7db
deon-gracias <deongracias1@gmail.com>
2023-05-15 20:54:10 +0530

    Added data

--- components/DrawerLayout.tsx
+++ components/DrawerLayout.tsx
@@ -1,30 +1,22 @@
 import { PropsWithChildren } from "react";
 import Header from "./Header";
+import { nav_items } from "../data/site";
 
 export default function DrawerLayout({ children }: PropsWithChildren) {
-  const navItems = [
-    { name: "Home", href: "#" },
-    { name: "Experience", href: "#experience" },
-    { name: "Education", href: "#education" },
-    { name: "Projects", href: "#projects" },
-  ];
-
   return (
     <div className="drawer">
       <input id="site-drawer" type="checkbox" className="drawer-toggle" />
       <div className="flex flex-col drawer-content">
-        <Header items={navItems} />
+        <Header items={nav_items} />
         {children}
       </div>
 
       <div className="drawer-side">
         <label htmlFor="site-drawer" className="drawer-overlay"></label>
         <ul className="p-4 menu w-80 bg-base-100">
-          {navItems.map((item) => (
+          {nav_items.map((item) => (
             <li key={item.name}>
-              <a href={item.href}>
-                {item.name}
-              </a>
+              <a href={item.href}>{item.name}</a>
             </li>
           ))}
         </ul>
