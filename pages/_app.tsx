4ffedfa7af31ca96b302f6d87f62764412c373f1
deon-gracias <deongracias1@gmail.com>
2023-05-16 02:18:44 +0530

    Added framer motion and custom cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { themeChange } from "theme-change";
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = { damping: 25, stiffness: 700 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
   useEffect(() => {
     themeChange(false);
+
+    const moveCursor = (e: MouseEvent) => {
+      cursorX.set(e.clientX - 16);
+      cursorY.set(e.clientY - 16);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        className="cursor"
+        style={{
+          translateX: cursorXSpring,
+          translateY: cursorYSpring,
+        }}
+      />
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
