03eaff3b9ffa9d55e9ec3eefcb2cc11119f47aa2
deon-gracias <deongracias1@gmail.com>
2023-05-21 03:56:05 +0530

    Displaying only first 2 projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,42 +168,44 @@ function Projects() {
       <SectionHeading className="flex justify-center" title="Projects" />
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {Object.entries(projects).map(([id, project]) => (
-          <div
-            className="duration-200 shadow-md card hover:shadow-xl"
-            key={`${project.title}`}
-          >
-            <div className="card-body">
-              <h2 className="card-title">{project.title}</h2>
-              <p>{project.description}</p>
-              <div className="flex gap-1 mb-2 flex-wrap">
-                {project.technologies.map((item) => (
-                  <div
-                    className="overflow-hidden duration-200 border rounded-full hover:border-primary"
-                    key={item}
-                  >
-                    <img
-                      className="w-full h-full"
-                      src={skill_badges_mono[item]}
-                      alt={item}
-                    />
-                  </div>
-                ))}
-              </div>
+        {Object.entries(projects)
+          .slice(0, 2)
+          .map(([id, project]) => (
+            <div
+              className="duration-200 shadow-md card hover:shadow-xl"
+              key={`${project.title}`}
+            >
+              <div className="card-body">
+                <h2 className="card-title">{project.title}</h2>
+                <p>{project.description}</p>
+                <div className="flex gap-1 mb-2 flex-wrap">
+                  {project.technologies.map((item) => (
+                    <div
+                      className="overflow-hidden duration-200 border rounded-full hover:border-primary"
+                      key={item}
+                    >
+                      <img
+                        className="w-full h-full"
+                        src={skill_badges_mono[item]}
+                        alt={item}
+                      />
+                    </div>
+                  ))}
+                </div>
 
-              <div className="card-actions justify-end">
-                <CursorObserver state={"link"}>
-                  <Link href={`/projects/${id}`}>
-                    <button className="btn btn-primary gap-2">
-                      <IconExternalLink />
-                      View
-                    </button>
-                  </Link>
-                </CursorObserver>
+                <div className="card-actions justify-end">
+                  <CursorObserver state={"link"}>
+                    <Link href={`/projects/${id}`}>
+                      <button className="btn btn-primary gap-2">
+                        <IconExternalLink />
+                        View
+                      </button>
+                    </Link>
+                  </CursorObserver>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </section>
   );
