6d3513ccc7c67f1e329a178529e027c126a10561
Deon Gracias <58945356+deon-gracias@users.noreply.github.com>
2024-03-27 11:09:56 +0530

    Changed default theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="light" className="scroll-pt-20">
+    <Html data-theme="lofi" className="scroll-pt-20">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
